cb9e1b8ba23e19c23c6df283beb5788706afb404
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2018-06-19 17:59:20 -0400

    Create Stat.sol
    Central Brand Factory/Registry initial list of imports for various token capabilities

---
+++ Stat.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17;
+
+// Central Brand Factory/Registry with Customizable Token Factory and Ion Registry
+
+import "./erc777.sol";
+import "./erc721.sol";
+import "./erc988.sol";
+import "./staking.sol";
+import "./freezable.sol";
+import "./totalSupplyLocked.sol";
+import "./ownable.sol";
+import "./clonable.sol";
+import "./mintable.sol";
+import "./tradable.sol";
+import "./stickable.sol";
+import "./taxonomy.sol";
+import "./whitelist.sol";
+import "./erc820IonHooks.sol";
+import "./lockable.sol";
+import "./dao.sol";
+import "./tradable.sol";
+
